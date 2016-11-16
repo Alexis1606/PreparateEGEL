2a6621d1be8216883376cef9fab2c72f3f90689c
alexis <alexis_1606@hotmail.com>
2016-11-15 19:08:46 -0600

    El link ya manda a la actividad correcta https://sz79t.app.goo.gl/Scq6

---
+++ app/src/main/java/calejo/preparateegel/Sorpresa.java
@@ -0,0 +1,13 @@
+package calejo.preparateegel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sorpresa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sorpresa);
+    }
+}
