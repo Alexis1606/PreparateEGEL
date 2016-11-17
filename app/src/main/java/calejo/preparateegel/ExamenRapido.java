5d0633d9ee06864f315f2668976f6ea96ecb6359
alexis <alexis_1606@hotmail.com>
2016-11-17 12:04:58 -0600

    Ya consulta la base de datos y llena los atibutos de la clase PreguntasServeceWeb

--- app/src/main/java/calejo/preparateegel/ExamenRapido.java
+++ app/src/main/java/calejo/preparateegel/ExamenRapido.java
@@ -3,11 +3,22 @@ package calejo.preparateegel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static calejo.preparateegel.PreguntasWebService.pregunta;
+
 public class ExamenRapido extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_examen_rapido);
+
+        PreguntasWebService pregunta;
+        pregunta = new PreguntasWebService();
+
+        pregunta.obtenerPregunta();
+        String preguntaA = pregunta.preguntaM;
+
+        int x;
+        x=0;
     }
 }
