4b31b213e7b962e6002fa4a605f6dd2b908384b7
Juan Alexis Romero Mora <alexis_1606@hotmail.com>
2016-11-21 01:42:40 -0600

    En examen completo       deselecciona el radio button anterior}       muestra la respuesta correcta en el alert dialog

--- app/src/main/java/calejo/preparateegel/ExamenCompleto.java
+++ app/src/main/java/calejo/preparateegel/ExamenCompleto.java
@@ -10,7 +10,6 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class ExamenCompleto extends AppCompatActivity {
-
     String rCorrecta;
     int cont =0;
     PreguntasWebService pregunta;
@@ -22,9 +21,11 @@ public class ExamenCompleto extends AppCompatActivity {
     int contadorIncorrecto = 0;
     @Override
 
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_examen_rapido);
+        setContentView(R.layout.activity_examen_completo);
 
         pregunta = new PreguntasWebService();
         pregunta.obtenerPregunta();
@@ -55,11 +56,6 @@ public class ExamenCompleto extends AppCompatActivity {
         rR4.setText(r4);
     }
 
-    public void salir() {
-        Intent intent = new Intent(this,Menu.class);
-        startActivity(intent);
-    }
-
     void setText(PreguntasWebService pregunta){
         String preguntaA = pregunta.preguntaM;
         String r1 = pregunta.respuesta1;
@@ -78,12 +74,15 @@ public class ExamenCompleto extends AppCompatActivity {
         rR4.setText(r4);
     }
 
-    public void siguiente(View view){
-        if(cont<70) {
-            pregunta = new PreguntasWebService();
-            pregunta.obtenerPregunta();
+    public void salir() {
+        Intent intent = new Intent(this,Menu.class);
+        startActivity(intent);
+    }
+
+    public void siguiente(View view) throws InterruptedException {
+        if(cont<70){
+
             this.setText(pregunta);
-            rCorrecta = pregunta.respuesta_correcta;
             if((rR1.isChecked() && rCorrecta.equals("a")) || (rR2.isChecked() && rCorrecta.equals("b"))  || (rR3.isChecked() && rCorrecta.equals("c")) || (rR4.isChecked() && rCorrecta.equals("d")) ){
                 AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
                 dlgAlert.setMessage("Respuesta Correcta");
@@ -92,31 +91,45 @@ public class ExamenCompleto extends AppCompatActivity {
                 dlgAlert.setCancelable(true);
                 dlgAlert.create().show();
                 contadorCorrecto++;
-                rR1.setSelected(false);
-                rR2.setSelected(false);
-                rR3.setSelected(false);
-                rR4.setSelected(false);
+                
             }else {
                 AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
-                dlgAlert.setMessage("Respuesta Incorrecta");
+                switch(rCorrecta){
+                    case "a":
+                        dlgAlert.setMessage("Respuesta Incorrecta\nRespuesta correcta: "+pregunta.respuesta1);
+                        break;
+                    case "b":
+                        dlgAlert.setMessage("Respuesta Incorrecta\nRespuesta correcta: "+pregunta.respuesta2);
+                        break;
+                    case "c":
+                        dlgAlert.setMessage("Respuesta Incorrecta\nRespuesta correcta: "+pregunta.respuesta3);
+                        break;
+                    case "d":
+                        dlgAlert.setMessage("Respuesta Incorrecta\nRespuesta correcta: "+pregunta.respuesta4);
+                        break;
+                }
+                //dlgAlert.setMessage("Respuesta Incorrecta\nRespuesta correcta: "+rCorrecta);
                 dlgAlert.setTitle("Preparate EGEL");
                 dlgAlert.setPositiveButton("OK", null);
                 dlgAlert.setCancelable(true);
                 dlgAlert.create().show();
                 contadorIncorrecto++;
-                rR1.setSelected(false);
-                rR2.setSelected(false);
-                rR3.setSelected(false);
-                rR4.setSelected(false);
+
+
+
             }
+            desseleccionar();
+            pregunta = new PreguntasWebService();
+            pregunta.obtenerPregunta();
+            this.setText(pregunta);
+            rCorrecta = pregunta.respuesta_correcta;
             cont ++;
         }else{
-            // Intent intent = new Intent(this,ExamenRapido.class);
-            //startActivity(intent);
+
             AlertDialog.Builder Respuesta  = new AlertDialog.Builder(this);
             Respuesta.setMessage("Respuestas correctas: "+ contadorCorrecto + "\n" + "Dato Curioso");
             Respuesta.setTitle("Preparate EGEL");
-            //Respuesta.setPositiveButton("OK", null );
+            // Respuesta.setPositiveButton("OK", null );
             Respuesta.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int id) {
                     salir();
@@ -125,5 +138,14 @@ public class ExamenCompleto extends AppCompatActivity {
             Respuesta.setCancelable(true);
             Respuesta.create().show();
         }
+
     }
+
+    public void desseleccionar(){
+        rR1.setChecked(false);
+        rR2.setChecked(false);
+        rR3.setChecked(false);
+        rR4.setChecked(false);
+    }
+
 }
